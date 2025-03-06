e28ea99ffbd10be6b0c3adb1b954fde1e9deb44f
Rubén Cancho <304053+Canx@users.noreply.github.com>
2025-03-06 17:11:23 +0100

    Mejorada instalación

--- install.sh
+++ install.sh
@@ -4,32 +4,26 @@
 USER="usuario"  # Cambia esto por tu nombre de usuario
 WORKING_DIR="$(pwd)"  # Establece el directorio actual como WORKING_DIR
 VENV_PATH="$WORKING_DIR/venv"
+VENV_UNO_PATH="$WORKING_DIR/venv_uno"
 GUNICORN_CMD="$VENV_PATH/bin/gunicorn -w 4 -b 127.0.0.1:8000 app:app"
 SERVICE_FILE="/etc/systemd/system/mi_app.service"
 
 # Instalar unoconv
 echo "Instalando libreoffice..."
-sudo add-apt-repository ppa:libreoffice/ppa
+sudo add-apt-repository -y ppa:libreoffice/ppa
 sudo apt update
-sudo apt install libreoffice
-sudo apt install python3-pip
-sudo apt install python3-venv
+sudo apt install -y libreoffice python3-pip python3-venv
 
 # Crear el entorno virtual si no existe
 if [ ! -d "$VENV_PATH" ]; then
     echo "Creando entorno virtual en $VENV_PATH"
-    python3 -m venv --system-site-packages "$VENV_PATH"
+    python3 -m venv --upgrade-deps "$VENV_PATH"
 fi
-
-# Activar el entorno virtual
-source "$VENV_PATH/bin/activate"
-
-
 
 # Instalar paquetes necesarios
 if [ -f "$WORKING_DIR/requirements.txt" ]; then
     echo "Instalando paquetes desde requirements.txt"
-    pip install -r "$WORKING_DIR/requirements.txt"
+    "$VENV_PATH/bin/pip" install -r "$WORKING_DIR/requirements.txt"
 else
     echo "No se encontró requirements.txt. Asegúrate de que el archivo esté en el directorio de la aplicación."
 fi
--- start_unoserver.sh
+++ start_unoserver.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export PYTHONPATH=/usr/lib/python3/dist-packages:$PYTHONPATH
 source ./venv/bin/activate
 unoserver
